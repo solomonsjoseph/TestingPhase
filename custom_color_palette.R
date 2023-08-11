dbfa62108fdd98511cad4a8ab73cd613542a742a
Solomon <sj1234520162016@gmail.com>
2023-08-11 10:55:50 -0400

    New update works for character data in se object! But reverted back the changes made to the legends!!

--- custom_color_palette.R
+++ custom_color_palette.R
@@ -16,3 +16,7 @@ create_color_palette <- function(n) {
 #n <- 100  # Change this value as you need
 #colors <- create_color_palette(n)
 #show_col(colors)
+
+#test_palette <- create_color_palette(5)
+#print(test_palette)
+
